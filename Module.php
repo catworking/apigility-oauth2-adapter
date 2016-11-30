dd23f48b84adfb3d67710d11318eee0f29d59a14
yangshinan <yangsn@figo.cn>
2016-11-30 16:45:58 +0800

    增加路由配置，由项目配置中的global.php转移过来

--- Module.php
+++ Module.php
@@ -15,9 +15,11 @@ class Module implements AutoloaderProviderInterface
     public function getConfig()
     {
         $doctrine_config = new Config(include __DIR__ . '/config/doctrine.config.php');
+        $manual_config = new Config(include __DIR__ . '/config/manual.config.php');
 
         $module_config = new Config(include __DIR__ . '/config/module.config.php');
         $module_config->merge($doctrine_config);
+        $module_config->merge($manual_config);
 
         return $module_config;
     }
---
+++ config/manual.config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: figo-007
+ * Date: 2016/11/30
+ * Time: 16:32
+ */
+return [
+    'router' => [
+        'routes' => [
+            'oauth' => [
+                'options' => [
+                    'spec' => '%oauth%',
+                    'regex' => '(?P<oauth>(/oauth))',
+                ],
+                'type' => 'regex',
+            ],
+        ],
+    ]
+];
